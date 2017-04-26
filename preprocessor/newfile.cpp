f5d076fd4740cc19ceb012e4bd5b056825389b75
thelonster <lg3hansen@gmail.com>
2017-04-26 10:09:29 -0700

    New file generated through program. Processed version of testfile.cpp without #define and #undef lines.

---
+++ preprocessor/newfile.cpp
@@ -0,0 +1,10 @@
+char abuf[] = "AU_CONTRAIRE_MON_CAPITAINE";
+int j = 1000, k = 2 * 1000;
+char anotherbbuf[] = "only reason I come is to listen to these wonderful speeches of yours";
+// this is a comment with PICARD in it -- it shouldn't have its defn replace it
+/* this is a comment */
+// this is a single line comment
+int i = 1000;
+const char* ps = "all they left me is my bones";
+// this is a comment with HASHSIZE in it -- it should not be replaced by its defn! */
+/* END OF FILE */
